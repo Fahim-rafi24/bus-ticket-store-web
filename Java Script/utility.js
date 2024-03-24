410098ad82d358aec25fdf44a80245ae6e7f4571
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-03-24 17:49:59 +0600

    js add

---
+++ Java Script/utility.js
@@ -0,0 +1,28 @@
+
+
+
+
+// // Get all button elements within the body
+// let buttons = document.querySelectorAll('body button');
+        
+// // Loop through each button and add the 'glass' class
+// buttons.forEach(function(button) {
+//     button.classList.add('glass');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
