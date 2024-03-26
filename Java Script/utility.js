7e8e4dc6f29a45b484f0194915cda3e987413b83
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-03-27 00:50:47 +0600

    set seat-btn

--- Java Script/utility.js
+++ Java Script/utility.js
@@ -20,7 +20,24 @@
 
 
 
-
+    function addNewDiv() {
+        // Create a new div element
+        var newDiv = document.createElement("div");
+        newDiv.classList.add("flex", "justify-between", "text-black", "border-b", "py-3", "text-left");
+        
+        // Add content to the new div
+        newDiv.innerHTML = "<!-- Content of your new div goes here -->";
+        
+        // Get the seat-booking-info div
+        var bookingInfoDiv = document.getElementById("seat-booking-info");
+        
+        // Append the new div to the seat-booking-info div
+        bookingInfoDiv.appendChild(newDiv);
+    }
+    
+    // Call the function to add the new div
+    
+    
 
 
 
